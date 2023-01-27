5cd7e40c9ce9559004e17969e897230f03d9135e
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2023-01-27 17:32:35 -0500

    steps order created

--- cypress/e2e/features/order.feature
+++ cypress/e2e/features/order.feature
@@ -5,5 +5,5 @@ Feature: Compra KFC Colombia
     Scenario: Compra exitosa
         Given se inicia sesion con usuario "standard_user" y clave "secret_sauce"
         When el usuario agrega a la canasta el producto "Sauce Labs Bike Light"
-        And diligencia la informacion del checkout con nombre "Julian" apellido "Novoa" y zip code "00000"
+        When diligencia la informacion del checkout con nombre "Julian" apellido "Novoa" y zip code "00000"
         Then La orden es creada exitosamente
--- cypress/e2e/step_definitions/order.js
+++ cypress/e2e/step_definitions/order.js
@@ -1,17 +1,39 @@
-import { Given, When, Then} from '@badeball/cypress-cucumber-preprocessor'
+import {
+  Given,
+  When,
+  Then,
+} from "@badeball/cypress-cucumber-preprocessor";
 
-Given("se inicia sesion con usuario {string} y clave {string}", (user, password)=>{
+Given(
+  "se inicia sesion con usuario {string} y clave {string}",
+  (user, password) => {
     cy.visit("https://www.saucedemo.com/");
     cy.get("#user-name").type(user);
     cy.get("#password").type(password);
     cy.get("#login-button").click();
-})
+  }
+);
 
 When("el usuario agrega a la canasta el producto {string}", (productName) => {
-    cy.contains(".inventory_item_name", productName).click();
-    cy.contains("button", 'Add to cart').click();
-    cy.get("#shopping_cart_container").click();
-    cy.get('[data-test="checkout"]').click();
-})
+  cy.contains(".inventory_item_name", productName).click();
+  cy.contains("button", "Add to cart").click();
+  cy.get("#shopping_cart_container").click();
+  cy.get('[data-test="checkout"]').click();
+});
 
-When("")
+When(
+  "diligencia la informacion del checkout con nombre {string} apellido {string} y zip code {string}",
+  (name, lasName, zipcode) => {
+    cy.get('[placeholder="First Name"]').type(name);
+    cy.get('[data-test="lastName"]').type(lasName);
+    cy.get("#postal-code").type(zipcode);
+    cy.get("#continue").click();
+  }
+);
+
+Then("La orden es creada exitosamente", () => {
+    cy.get('[data-test="finish"]').click();
+    cy.get(".complete-header").should($element => {
+        expect($element).to.have.text('THANK YOU FOR YOUR ORDER')
+    })
+})
